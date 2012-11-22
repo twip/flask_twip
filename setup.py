6e722e26b8c2dfa3f54a020d0cdf994816629784
yegle <cnyegle@gmail.com>
2012-11-22 00:38:40 -0500

    Update: version 0.0.4
    Added: SQLBackend. Dependency on SQLAlchemy is added for this backend
    Updated: example for heroku, use SQLBackend instead of FileBackend

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-Twip',
-    version='0.0.3',
+    version='0.0.4',
     url='https://github.com/yegle/flask_twip/',
     license='MPL',
     author='yegle',
